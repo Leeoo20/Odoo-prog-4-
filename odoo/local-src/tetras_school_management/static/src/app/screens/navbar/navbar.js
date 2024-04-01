64bd31b7d7269e13f235375cdb343d458d0d722f
matski <leovernier2003@gmail.com>
2024-04-01 16:48:20 +0200

    CSS / Responsive

--- odoo/local-src/tetras_school_management/static/src/app/screens/navbar/navbar.js
+++ odoo/local-src/tetras_school_management/static/src/app/screens/navbar/navbar.js
@@ -8,5 +8,23 @@ import { useTetras } from "@tetras_school_management/app/store/tetras_hook";
 export class Navbar extends Component {
     static template = "tetras_school_management.Navbar";
 
+    setup() {
+        this.tetras = useTetras();
+    }
+
+    changeScreen(ev){
+        console.log(ev);
+        const buttons = ev.target.offsetParent.querySelectorAll('a');
+            buttons.forEach(button => {
+                button.classList.remove('active');
+            });
+
+
+
+        const button = ev.explicitOriginalTarget;
+        button.classList.add('active');
+        this.tetras.showScreen(button.id);
+    }
+
 }
 
